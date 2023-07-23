a3f5c6bda834ebd06ecc00b2e5ae1089265dce61
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-24 00:42:55 +0300

    uploading eidtor

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 // // Live Code Eeditor
 
+var j=0;
+//Function for live Rendering
+function update(i) {
+    if(i==0){
+    let htmlCode=document.getElementById("htmlCode").value;
+    let cssCode=document.getElementById("cssCode").value;
+    let javascriptCode=document.getElementById("javascriptCode").value;
+    let text=htmlCode+"<style>"+cssCode+"</style>"+"<scri"+"pt>"+javascriptCode+"</scri"+"pt>";
+    let iframe=document.getElementById('viewer').contentWindow.document;
+    iframe.open();
+    iframe.write(text);
+    iframe.close();
+    }
 
+    else if(i==1){
 
+        let htmlCode=document.getElementById("htmlCode").value;
+        let html=htmlCode.slice(0,htmlCode.length);
+        document.getElementById("htmlCode").value=html;
+        j=1;
+
+    }
+}
 
 
 // // Nice Scrolling
